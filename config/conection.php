1baae75836b7d6690f5a74b03939fe4d4dcdcdd4
Alfredo <alfmorapime@gmail.com>
2023-10-05 15:40:38 -0700

    adding config folder and files

---
+++ config/conection.php
@@ -0,0 +1,8 @@
+<?php
+
+//$conection = mysqli_connect('localhost','root','','mrkadosc_db');
+//$conection = mysqli_connect("localhost", "websor", "st78ellar%HH7.", "stellarwholesaleinc", "3306");
+$conection = mysqli_connect("127.0.0.1", "root", "", "stellar wholesale inc", "3308");
+return $conection;
+
+?>
