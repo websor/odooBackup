db99abfe01a5a353e138ce5c6f6c34bea002c07b
Alfredo <alfmorapime@gmail.com>
2023-11-09 13:54:38 -0800

    conection file fixed

--- config/conection.php
+++ config/conection.php
@@ -3,7 +3,7 @@
 //$conection = mysqli_connect('_SERVER_','_USER_','_PASSWORD_','_DB_NAME_','_PORT_');
 //$conection = mysqli_connect("localhost", "websor", "st78ellar%HH7.", "stellarwholesaleinc", "3306");
 //$conection = mysqli_connect("127.0.0.1", "root", "", "stellar wholesale inc", "3308"); //LOCAL VARIABLE
-$conection = mysqli_connect("odoo.biofloral.com", "bioflora_odoouser", "LwrbtV5iivNLwrbtV5iivN", "bioflora_odoo", "3308"); //SERVER
+$conection = mysqli_connect("odoo.biofloral.com", "bioflora_odoouser", "LwrbtV5iivNLwrbtV5iivN", "bioflora_odoo"); //SERVER
 
 return $conection;
 
